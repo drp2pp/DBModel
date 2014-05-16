c2b0ff995536009f7028d137c3843b17328d9a61
hyojin kim <drp2pp@gmail.com>
2014-05-16 16:16:13 +0900

    140516

---
+++ ProjectDBTemp/src/service/domain/Beacon.java
@@ -0,0 +1,29 @@
+package service.domain;
+
+public class Beacon {
+	
+	private String uuid;
+	private int major;
+	private int minor;
+	
+	public String getUuid() {
+		return uuid;
+	}
+	public void setUuid(String uuid) {
+		this.uuid = uuid;
+	}
+	public int getMajor() {
+		return major;
+	}
+	public void setMajor(int major) {
+		this.major = major;
+	}
+	public int getMinor() {
+		return minor;
+	}
+	public void setMinor(int minor) {
+		this.minor = minor;
+	}
+	
+	
+}
---
+++ ProjectDBTemp/src/service/domain/Local.java
@@ -0,0 +1,29 @@
+package service.domain;
+
+public class Local {
+	
+	private String localName; 
+	private String gpsX;
+	private String gpsY;
+	
+	public String getLocalName() {
+		return localName;
+	}
+	public void setLocalName(String localName) {
+		this.localName = localName;
+	}
+	public String getGpsX() {
+		return gpsX;
+	}
+	public void setGpsX(String gpsX) {
+		this.gpsX = gpsX;
+	}
+	public String getGpsY() {
+		return gpsY;
+	}
+	public void setGpsY(String gpsY) {
+		this.gpsY = gpsY;
+	}
+	
+	
+}
