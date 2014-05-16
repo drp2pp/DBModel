12e2a860f4d8d670986b413539ba7b0b1f7a557d
hyojin kim <drp2pp@gmail.com>
2014-05-16 16:20:51 +0900

    140516

---
+++ ProjectDBTemp/src/service/impl/TourDaoImpl.java
@@ -0,0 +1,34 @@
+package service.impl;
+
+import java.util.List;
+
+import service.TourDao;
+import service.domain.Beacon;
+
+public class TourDaoImpl implements TourDao {
+
+	@Override
+	public List<Object> getLocalInfo() throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Object> getDataByLocal(int localNo) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Object> getDataByBeacon(int regionNo) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Beacon findBeacon(Beacon beacon) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ TestExerd/data.sql
@@ -0,0 +1,43 @@
+DROP SEQUENCE TOUR_LOCAL;
+
+DROP SEQUENCE TOUR_REGION;
+
+DROP SEQUENCE TOUR_DATA;
+
+CREATE SEQUENCE TOUR_LOCAL INCREMENT BY 1 START WITH 1;
+
+CREATE SEQUENCE TOUR_REGION INCREMENT BY 1 START WITH 1;
+
+CREATE SEQUENCE TOUR_DATA INCREMENT BY 1 START WITH 1;
+
+
+INSERT INTO local_info (local_no, manager_id, local_name, gps_x, gps_y)
+VALUES (TOUR_LOCAL.nextval, 'manager1', '광화문', '37.576037','126.9769');
+
+INSERT INTO local_info (local_no, manager_id, local_name, gps_x, gps_y)
+VALUES (TOUR_LOCAL.nextval, 'manager2', '흥례문', '37.576955','126.976884');
+
+INSERT INTO local_info (local_no, manager_id, local_name, gps_x, gps_y)
+VALUES (TOUR_LOCAL.nextval, 'manager3', '근정전', '37.578566','126.976954');
+
+INSERT INTO local_info (local_no, manager_id, local_name, gps_x, gps_y)
+VALUES (TOUR_LOCAL.nextval, 'manager4', '사정전', '37.579102','126.977008');
+
+INSERT INTO local_info (local_no, manager_id, local_name, gps_x, gps_y)
+VALUES (TOUR_LOCAL.nextval, 'manager5', '침전', '37.579561','126.97704');
+
+INSERT INTO local_info (local_no, manager_id, local_name, gps_x, gps_y)
+VALUES (TOUR_LOCAL.nextval, 'manager6', '향원전', '37.582337','126.977008');
+
+INSERT INTO local_info (local_no, manager_id, local_name, gps_x, gps_y)
+VALUES (TOUR_LOCAL.nextval, 'manager7', '경회루', '37.579736','126.975956');
+
+INSERT INTO local_info (local_no, manager_id, local_name, gps_x, gps_y)
+VALUES (TOUR_LOCAL.nextval, 'manager7', '동궁', '37.578911','126.978172');
+
+
+
+
+/*INSERT INTO region_info (region_no, region_name, uuid, major, minor, local_no)
+VALUES (TOUR_REGION.nextval, );
+*/
